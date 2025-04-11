044f3d19aaa072077246bdf3a2093425bccbc37e
Charly-bite <charly-bite@users.noreply.github.com>
2025-04-11 11:26:35 -0600

    Create requirements.sh
    Execute to install requirements

---
+++ requirements.sh
@@ -0,0 +1,4 @@
+pip install pandas beautifulsoup4 nltk scikit-learn matplotlib joblib shap lime tensorflow # Add alibi if using Anchor
+# Download spacy model if using Anchor
+# pip install spacy
+# python -m spacy download en_core_web_sm
